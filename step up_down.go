f92b4fccb618f6da872e3d4d2773585a0ac7ded4
wunhand <davidewcasey@gmail.com>
2025-02-24 04:44:04 -0800

    more batch files

---
+++ step up_down.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int = 0
+	fmt.Println(a); a++; fmt.Println(a); a--; fmt.Println(a)
+}
---
+++ subtraction_math.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("4320-2030=", 4320-2030)
+}
